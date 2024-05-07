21a81fadcd48f7a935c59c6c973ab23c5380d9fd
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2024-05-07 22:40:10 +0300

    added wallets and transactions

--- src/app/models/transaction.model.ts
+++ src/app/models/transaction.model.ts
@@ -1,7 +1,13 @@
-export interface MonthlyIncome {
+export interface Transaction {
     amount: number;
-    category: string;
+    category?: string;
     description: string;
+<<<<<<< HEAD
+    budgetName?: string;
+    transactionType: "Income" | "Expense";
+    date: Date;
+=======
     is_an_expense: 'boolean';
     time: Date;
+>>>>>>> 7a8f009c72db2f0dd318f7c6b378b49f3e2ce417
 }
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -3,4 +3,15 @@ export interface User {
     email: string;
     displayName?: string;
     photoURL?: string;
+    mainWalletAmount: number;
+    savingWalletAmount: number;
 }
+
+export const defaultUser: User = {
+    uid: '',
+    email: '',
+    displayName: '',
+    photoURL: 'https://static.vecteezy.com/system/resources/previews/009/292/244/original/default-avatar-icon-of-social-media-user-vector.jpg', 
+    mainWalletAmount: 0,
+    savingWalletAmount: 0
+};
