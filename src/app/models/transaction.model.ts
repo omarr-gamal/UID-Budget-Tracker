66e389f368c863d66a6e023bb401f82ca35b731a
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2024-05-08 02:49:54 +0300

    added crud for transactions

--- src/app/models/transaction.model.ts
+++ src/app/models/transaction.model.ts
@@ -2,12 +2,7 @@ export interface Transaction {
     amount: number;
     category?: string;
     description: string;
-<<<<<<< HEAD
     budgetName?: string;
     transactionType: "Income" | "Expense";
     date: Date;
-=======
-    is_an_expense: 'boolean';
-    time: Date;
->>>>>>> 7a8f009c72db2f0dd318f7c6b378b49f3e2ce417
 }
---
+++ src/app/services/user-transactions.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+
+import { AuthService } from './auth.service';
+import { Transaction } from '../models/transaction.model';
+
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { 
+  Observable, from,
+  map, switchMap, take 
+} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserTransactionsService {
+
+  constructor(
+    private auth: AuthService,
+    private afs: AngularFirestore
+  ) { }
+
+  addTransaction(transaction: Transaction): Observable<String> {
+    return this.auth.user$.pipe(
+      take(1),
+      switchMap(user => {
+        if (!user) {
+          return new Observable<never>(observer => {
+            observer.error('User not authenticated');
+          });
+        }
+        
+        const userId = user.uid;
+        const transactions = this.afs.collection(`users/${userId}/transactions`);
+        return from(transactions.add(transaction)).pipe(
+          map((docRef) => {
+            return docRef.id;
+          })
+        );
+      })
+    );
+  }
+
+  getAllTransactions(): Observable<Transaction[]> {
+    return this.auth.user$.pipe(
+      switchMap(user => {
+        if (!user) {
+          return new Observable<never>(observer => {
+            observer.error('User not authenticated');
+          });
+        }
+
+        const userId = user.uid;
+        const transactions = this.afs.collection<Transaction>(`users/${userId}/transactions`);
+
+        return transactions.valueChanges({ idField: 'id' });
+      })
+    )
+  }
+
+  updateTransaction(transaction_id: string, changes: Partial<Transaction>): Observable<Boolean> {
+    return this.auth.user$.pipe(
+      take(1),
+      switchMap(user => {
+        if (!user) {
+          return new Observable<never>(observer => {
+            observer.error('User not authenticated');
+          });
+        }
+
+        const userId = user.uid;
+        const transactions = this.afs.collection<Transaction>(`users/${userId}/transactions`);
+
+        return from(transactions.doc(transaction_id).update(changes)).pipe(
+          map(() => true)
+        );
+      })
+    )
+  }
+
+  deleteTransaction(transaction_id: string): Observable<Boolean> {
+    return this.auth.user$.pipe(
+      take(1),
+      switchMap(user => {
+        if (!user) {
+          return new Observable<never>(observer => {
+            observer.error('User not authenticated');
+          });
+        }
+
+        const userId = user.uid;
+        const transactions = this.afs.collection<Transaction>(`users/${userId}/transactions`);
+
+        return from(transactions.doc(transaction_id).delete()).pipe(
+          map(() => true)
+        );
+      })
+    )
+  }
+}
