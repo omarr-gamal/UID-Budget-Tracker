1474ca9a9603734d8fa667e7d6886a54d21de631
Omar Gamal AbdelMageed <56756748+fiend361@users.noreply.github.com>
2024-05-07 23:53:03 +0300

    added addBudget()

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { MonthlyIncome } from '../models/monthly-income.model';
-import { Observable, filter, first, from, switchMap, take } from 'rxjs';
-import { addDoc } from '@angular/fire/firestore';
+import { Observable, catchError, filter, first, from, map, switchMap, take } from 'rxjs';
+import { Budget } from '../models/budget.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,25 @@ export class UserService {
     private auth: AuthService,
     private afs: AngularFirestore
   ) { }
+
+  addBudget(budget: Budget): Observable<Boolean> {
+    return this.auth.user$.pipe(
+      take(1),
+      switchMap(user => {
+        if (!user) {
+          return new Observable<never>(observer => {
+            observer.error('User not authenticated');
+          });
+        }
+        
+        const userId = user.uid;
+        const coll = this.afs.collection(`users/${userId}/budgets`);
+        return from(coll.add(budget)).pipe(
+          map(() => true)
+        );
+      })
+    )
+  }
 
   addMonthlyIncome(monthlyIncome: MonthlyIncome): Observable<any> {
     return this.auth.user$.pipe(
@@ -26,7 +45,7 @@ export class UserService {
         }
         // add a new monthly income document
         const userId = user.uid;
-        const coll = this.afs.collection(`users/${userId}/monthly_income`);
+        const coll = this.afs.collection(`users/${userId}/monthly_incomes`);
         return from(coll.add(monthlyIncome)).pipe(first());
       })
     );
