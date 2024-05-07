be48a831dd075997a8f81c4b08050c01e25906a7
Omar Emad <2001omaremad@gmail.com>
2024-05-07 20:42:29 +0300

    removed bootstrap, added primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,11 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 
+//primeng components
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,12 +35,16 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     FontAwesomeModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
+    //primeng
+    MenubarModule,
+    ButtonModule
   ],
   providers: [
     { provide: FIREBASE_OPTIONS, useValue: environment.firebase }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,44 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  items: MenuItem[] = [];
+  user: any;
   constructor(public auth: AuthService) { }
+
+
+  ngOnInit() {
+    this.auth.user$.subscribe(user => {
+      this.user = user;
+      this.updateMenuItems();
+    });
+  }
+
+  updateMenuItems() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        routerLink: ['/home']
+      },
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-fw pi-chart-line',
+        routerLink: ['/dashboard']
+      }
+    ];
+
+    if (this.user) {
+      this.items.push({
+        label: this.user.displayName,
+        items: [
+          {
+            label: 'Logout',
+            icon: 'pi pi-fw pi-sign-out',
+            command: () => this.auth.signOut()
+          }
+        ]
+      });
+    }
+  }
 
 }
